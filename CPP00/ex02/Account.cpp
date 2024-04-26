4349b2886bfff3936fe6c600ae8f94b4d41ebb8f
vasco <vascosoares40@gmail.com>
2024-04-26 05:13:05 +0100

    done

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -50,7 +50,52 @@ void Account::displayAccountsInfos(void){
     cout << "accounts" << _nbAccounts << "total:" << _totalAmount << "deposits:" << _totalNbDeposits << "withdrawls:" << _totalNbWithdrawals << endl;
 }
 
-void Account::makeDeposit(void)
-{
-    cout << "index:" << _accountIndex << 
+void Account::makeDeposit(int deposit) {
+    _nbDeposits++;
+    _displayTimestamp();
+    cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";deposit:" << deposit << ";amount:" << _amount + deposit << ";nb_deposit:" << _nbDeposits << endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+    if (_amount - withdrawal < 0)
+    {
+        _displayTimestamp();
+        cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:refused" << endl;
+        return false;
+    }
+    _nbWithdrawals++;
+    _displayTimestamp();
+    cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:" << withdrawal << ";amount:" << _amount - withdrawal << ";nb_withdrawals:" << _nbWithdrawals << endl;
+    _amount -= withdrawal;
+    _totalAmount -= withdrawal;
+    _totalNbWithdrawals++;
+    return true;
+}
+
+int Account::checkAmount(void) const {
+    return _amount;
+}
+
+void Account::displayStatus(void) const {
+    _displayTimestamp();
+    cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << endl;
+}
+
+void Account::_displayTimestamp(void) {
+    time_t result;
+    /* guarda componentes de um calendario: ano, mes, dia, hora, minuto e segundo */
+    struct tm *time_stamp;
+
+    /* tempo atual em segundos desde o epoch */
+    std::time(&result);
+    /* transforma em [YYYYMMDD_HHMMSS] */
+    time_stamp = localtime(&result);
+    cout << "[";
+    cout << time_stamp->tm_year + 1900;
+    cout << setw(2) << setfill('0') << time_stamp->tm_mon + 1;
+    cout << setw(2) << setfill('0') << time_stamp->tm_mday << "_";
+    cout << setw(2) << setfill('0') << time_stamp->tm_hour;
+    cout << setw(2) << setfill('0') << time_stamp->tm_min;
+    cout << setw(2) << setfill('0') << time_stamp->tm_sec;
+    cout << "] ";
 }
